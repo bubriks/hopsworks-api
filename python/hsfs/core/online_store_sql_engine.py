c4f793fd683122775a120e93a5479786754c4654
bubriks <bubriks@gmail.com>
2024-08-28 11:47:44 +0300

    transaction?

--- python/hsfs/core/online_store_sql_engine.py
+++ python/hsfs/core/online_store_sql_engine.py
@@ -562,19 +562,20 @@ class OnlineStoreSqlClient:
                 len(self._prepared_statements[self.SINGLE_VECTOR_KEY])
             )
         async with self._connection_pool.acquire() as conn:
-            # Execute the prepared statement
-            _logger.debug(
-                f"Executing prepared statement: {stmt} with bind params: {bind_params}"
-            )
-            cursor = await conn.execute(stmt, bind_params)
-            # Fetch the result
-            _logger.debug("Waiting for resultset.")
-            resultset = await cursor.fetchall()
-            _logger.debug(f"Retrieved resultset: {resultset}. Closing cursor.")
-            print(1)
-            await cursor.close()
-            await conn.commit()
-            print(2)
+            async with conn.begin() as transaction:
+                # Execute the prepared statement
+                _logger.debug(
+                    f"Executing prepared statement: {stmt} with bind params: {bind_params}"
+                )
+                cursor = await conn.execute(stmt, bind_params)
+                # Fetch the result
+                _logger.debug("Waiting for resultset.")
+                resultset = await cursor.fetchall()
+                _logger.debug(f"Retrieved resultset: {resultset}. Closing cursor.")
+                print(1)
+                await transaction.commit()
+                await cursor.close()
+                print(2)
 
         print(3)
         return resultset
