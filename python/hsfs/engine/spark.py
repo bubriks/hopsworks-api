21d849a0491d86b30a15e6ab0e8272952e51b0ac
bubriks <bubriks@gmail.com>
2024-09-18 12:28:42 +0300

    tttt

--- python/hsfs/engine/spark.py
+++ python/hsfs/engine/spark.py
@@ -484,6 +484,7 @@ class Engine:
                 self._spark_context,
             )
 
+            feature_group._location = self.setup_storage_connector(feature_group.storage_connector, feature_group.path)
             hudi_engine_instance.save_hudi_fg(
                 dataframe, self.APPEND, operation, write_options, validation_id
             )
@@ -495,7 +496,6 @@ class Engine:
                 self._spark_session,
                 self._spark_context,
             )
-            feature_group._location = self.setup_storage_connector(feature_group.storage_connector, feature_group.path)
             delta_engine_instance.save_delta_fg(dataframe, write_options, validation_id)
         else:
             dataframe.write.format(self.HIVE_FORMAT).mode(self.APPEND).options(
